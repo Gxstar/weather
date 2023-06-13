d6cde148a6b668448f1c3939d198448dbd815cbf
gxstar <gengxing123@qq.com>
2023-06-13 23:55:57 +0800

    添加模版文件夹

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello world!"
+    wea={
+        'weather': 'sun',
+        'wind': 6,
+        'wet': 15
+    }
+    return render_template('index.html',wea=wea)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug=True
+    app.run(use_reloader=False)
 
