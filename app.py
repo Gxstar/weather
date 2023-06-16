c338d3bfd792b70aae56a2c2cc58927c88c6703c
gxstar <gengxing123@qq.com>
2023-06-17 00:34:58 +0800

    IP转经纬度（高德api）

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def home(): 
+    key='6c5dc70b8be20a22302470f4576304b4'
+    if request.remote_addr=='127.0.0.1':
+        ip='118.120.121.59'
+    else:
+        ip=request.remote_addr
+    url='https://restapi.amap.com/v3/ip?ip='+ip+'&key='+key
+    info=requests.get(url)
     wea={
         'weather': 'sun',
         'wind': 6,
-        'wet': 15
+        'wet': 15,
+        'ip': ip
     }
+
     return render_template('index.html',wea=wea)
 
 if __name__ == '__main__':
